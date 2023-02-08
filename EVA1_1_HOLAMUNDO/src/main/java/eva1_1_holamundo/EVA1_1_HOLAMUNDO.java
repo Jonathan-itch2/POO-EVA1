1c5426efa05833f29a3eea5e82aee6519a581abe
Jonathan-itch2 <124762544+Jonathan-itch2@users.noreply.github.com>
2023-02-08 00:06:55 -0600

    Add files via upload

---
+++ EVA1_1_HOLAMUNDO/src/main/java/eva1_1_holamundo/EVA1_1_HOLAMUNDO.java
@@ -0,0 +1,11 @@
+//Alumno: Jonathan David Salazar Vazquez 22550321
+//Materia: Programacion orientada a objetos 
+//Docente: Hernandez Chavez Ruben Alonso
+package eva1_1_holamundo;
+
+public class EVA1_1_HOLAMUNDO {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo !!");
+    }
+}
