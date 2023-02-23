5c70b06210d1403a5b02bdfa7bf7beb383bfc7e2
Jonathan-itch2 <124762544+Jonathan-itch2@users.noreply.github.com>
2023-02-22 19:48:56 -0600

    Add files via upload

---
+++ EVA1_11_RETURN/src/main/java/eva1_11_return/EVA1_11_RETURN.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package eva1_11_return;
+
+/**
+ *
+ * @author jonat
+ */
+public class EVA1_11_RETURN {
+
+    public static void main(String[] args) {
+        int resu = square(10);
+        System.out.println("Resultado: "+ resu);
+    }
+    public static int square(int num){
+        return num*num;
+    }
+}
