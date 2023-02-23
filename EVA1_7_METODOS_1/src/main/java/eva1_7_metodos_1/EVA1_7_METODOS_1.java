a2cf0c8712ece1b6950e0c2831b401aa56564a4e
Jonathan-itch2 <124762544+Jonathan-itch2@users.noreply.github.com>
2023-02-22 19:45:20 -0600

    Add files via upload

---
+++ EVA1_7_METODOS_1/src/main/java/eva1_7_metodos_1/EVA1_7_METODOS_1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package eva1_7_metodos_1;
+
+/**
+ *
+ * @author jonat
+ */
+public class EVA1_7_METODOS_1 {
+
+    public static void main(String[] args) {
+        myMethod();
+    }
+    public static void myMethod(){
+        System.out.println("You have called me: my name is myMethod!!");
+    }
+}
