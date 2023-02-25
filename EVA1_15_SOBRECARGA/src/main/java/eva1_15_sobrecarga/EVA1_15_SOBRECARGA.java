64acf27b784fadc77a35167d38a9fb3763685774
Jonathan-itch2 <124762544+Jonathan-itch2@users.noreply.github.com>
2023-02-25 01:22:07 -0600

    Add files via upload

---
+++ EVA1_15_SOBRECARGA/src/main/java/eva1_15_sobrecarga/EVA1_15_SOBRECARGA.java
@@ -0,0 +1,20 @@
+package eva1_15_sobrecarga;
+
+public class EVA1_15_SOBRECARGA {
+
+    public static void main(String[] args) {
+         /*
+        DIFERENTES PRINTLN: SON 10 DIFERENTES
+        METODOS PARA IMPRIMIR DIFERENTES TIPOS DE DATOS
+        */
+        /*
+        PARA CREAR SOBRECARGA DE METODOS:
+        LA FIRMA DEL METODO PUEDE SER UNICA
+        */
+        
+        System.out.println("Hola mundo!!");
+        System.out.println(4444);
+        System.out.println(true);
+        System.out.println(args);
+    }
+}
